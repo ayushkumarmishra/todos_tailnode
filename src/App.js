742dd19684b76d6c93bffb1d3768bacef8a6116e
Ayush Kumar Mishra <mishraayushsec@gmail.com>
2022-06-12 18:51:07 +0530

    unwanted_files deleted

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Header from "./component/Header";
+import TaskForm from "./component/TaskForm";
+import TaskList from "./component/TaskList";
+import { TasklistContextProvider } from "./context/Tasklist";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TasklistContextProvider>
+      <div className="container">
+        <div className="app-wrapper">
+          <Header />
+          <div className="main">
+            <TaskForm />
+            <TaskList />
+          </div>
+        </div>
+      </div>
+    </TasklistContextProvider>
   );
 }
 
